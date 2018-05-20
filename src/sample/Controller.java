36392b4b36b9522079102f50eb2c96ab74debf59
Jonathan Kuhl <jonathankuhl@Jonathans-MBP.maine.rr.com>
2018-05-19 21:42:38 -0400

    user interface updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
 public class Controller {
+
+    public void handleClose(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
